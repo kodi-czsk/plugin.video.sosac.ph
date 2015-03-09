71c931824f069edbc767324c8257dc7c698d827a
Libor Zoubek <lzoubek@redhat.com>
2015-03-09 21:33:14 +0100

    fix install location to .kodi

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 # do NOT use this script from XBMC addons directory, it is intented for development only
-DESTDIR=~/.xbmc/addons/plugin.video.sosac.ph
+DESTDIR=~/.kodi/addons/plugin.video.sosac.ph
 
 rm -rf ${DESTDIR}
 mkdir -p ${DESTDIR}
