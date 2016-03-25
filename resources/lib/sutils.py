8ad0c9507ee5b38cb501f6afabb026bca782127a
Lubomir Kucera <lubomir.kucera.jr@gmail.com>
2016-03-25 14:01:16 +0100

    sutils.py: Code cleanup

--- resources/lib/sutils.py
+++ resources/lib/sutils.py
@@ -1,12 +1,24 @@
-import util,xbmcprovider,xbmcutil,xbmcvfs,xbmcgui,xbmc
-import unicodedata,os,re,time,string,datetime,urllib
+import util
+import xbmcprovider
+import xbmcutil
+import xbmcvfs
+import xbmcgui
+import xbmc
+import unicodedata
+import os
+import re
+import time
+import string
+import datetime
+import urllib
+
 
 class XBMCSosac(xbmcprovider.XBMCMultiResolverContentProvider):
     last_run = 0
     sleep_time = 1000 * 10 * 60
-    
-    def __init__(self,provider,settings,addon):
-        xbmcprovider.XBMCMultiResolverContentProvider.__init__(self,provider,settings,addon)
+
+    def __init__(self, provider, settings, addon):
+        xbmcprovider.XBMCMultiResolverContentProvider.__init__(self, provider, settings, addon)
         provider.parent = self
         self.dialog = xbmcgui.DialogProgress()
         try:
@@ -14,18 +26,19 @@ class XBMCSosac(xbmcprovider.XBMCMultiResolverContentProvider):
             self.cache = StorageServer.StorageServer("Downloader")
         except:
             import storageserverdummy as StorageServer
-            self.cache = StorageServer.StorageServer("Downloader") 
+            self.cache = StorageServer.StorageServer("Downloader")
 
     def make_name(self, text, lower=True):
         text = self.normalize_filename(text, "-_.' %s%s" % (string.ascii_letters, string.digits))
         word_re = re.compile(r'\b\w+\b')
-        text = ''.join([c for c in text if (c.isalnum() or c=="'" or c == '.' or c =='-' or c.isspace())]) if text else ''
+        text = ''.join([c for c in text if (c.isalnum() or c == "'" or c ==
+                                            '.' or c == '-' or c.isspace())]) if text else ''
         text = '-'.join(word_re.findall(text))
         return text.lower() if lower else text
 
-    def normalize_filename(self,name,validChars=None):
+    def normalize_filename(self, name, validChars=None):
         validFilenameChars = "-_.() %s%s" % (string.ascii_letters, string.digits)
-        if (validChars != None):
+        if (validChars is not None):
             validFilenameChars = validChars
         cleanedFilename = self.encode(name)
         return ''.join(c for c in cleanedFilename if c in validFilenameChars)
@@ -37,18 +50,18 @@ class XBMCSosac(xbmcprovider.XBMCMultiResolverContentProvider):
         except:
             sleep_time = self.sleep_time
             pass
-        
+
         xbmc.sleep(sleep_time)
         try:
-            self.last_run = float(self.cache.get("subscription.last_run")) #time.time()
+            self.last_run = float(self.cache.get("subscription.last_run"))  # time.time()
         except:
             self.last_run = time.time()
             self.cache.set("subscription.last_run", str(self.last_run))
             pass
-        
+
         if time.time() > self.last_run + 24 * 3600:
-            self.evalSchedules();
-            
+            self.evalSchedules()
+
         while(not xbmc.abortRequested):
             if(time.time() > self.last_run + 24 * 3600):
                 self.evalSchedules()
@@ -58,12 +71,14 @@ class XBMCSosac(xbmcprovider.XBMCMultiResolverContentProvider):
             xbmc.sleep(self.sleep_time)
         util.info("Koncim")
 
-    def showNotification(self,title,message,time=1000):
-        xbmcgui.Dialog().notification(self.encode(title),self.encode(message),time=time,icon=xbmc.translatePath(self.addon_dir() + "/icon.png"),sound=False)
+    def showNotification(self, title, message, time=1000):
+        xbmcgui.Dialog().notification(self.encode(title), self.encode(message), time=time,
+                                      icon=xbmc.translatePath(self.addon_dir() + "/icon.png"),
+                                      sound=False)
 
     def evalSchedules(self):
-        if self.scanRunning() == False and self.isPlaying() == False:
-            self.showNotification('Subscription','Chcecking')
+        if not self.scanRunning() and not self.isPlaying():
+            self.showNotification('Subscription', 'Chcecking')
             util.info("Spustam co mam naplanovane")
             subs = self.get_subs()
             new_items = False
@@ -72,89 +87,104 @@ class XBMCSosac(xbmcprovider.XBMCMultiResolverContentProvider):
                     if self.scanRunning() or self.isPlaying():
                         self.cache.delete("subscription.last_run")
                         return
-                    new_items |= self.run_custom({'action': 'add-to-library', 'name': name, 'url': url, 'update': True})
+                    new_items |= self.run_custom(
+                        {'action': 'add-to-library', 'name': name, 'url': url, 'update': True})
                     xbmc.sleep(3000)
             if new_items:
                 xbmc.executebuiltin('UpdateLibrary(video)')
         else:
             util.info("Nieco srotuje, tak nic nerobim")
-    
+
     def isPlaying(self):
-        return xbmc.Player().isPlaying() == True
-    
+        return xbmc.Player().isPlaying()
+
     def scanRunning(self):
-        if(xbmc.getCondVisibility('Library.IsScanningVideo') or xbmc.getCondVisibility('Library.IsScanningMusic')):
-            return True
-        else:
-            return False
-    
+        return (xbmc.getCondVisibility('Library.IsScanningVideo') or
+                xbmc.getCondVisibility('Library.IsScanningMusic'))
+
     def getBBDB(self, name):
-        name = util.request('http://csfd.bbaron.sk/find.php?' + urllib.urlencode({'sosac': 1, 'name': name}))
+        name = util.request('http://csfd.bbaron.sk/find.php?' +
+                            urllib.urlencode({'sosac': 1, 'name': name}))
         if name != '':
             return self.getTVDB(name, 1)
         return None
-        
+
     def getTVDB(self, name, level=0):
-        data = util.request('http://thetvdb.com/api/GetSeries.php?' + urllib.urlencode({'seriesname': name, 'language':'cs'}))
+        data = util.request('http://thetvdb.com/api/GetSeries.php?' +
+                            urllib.urlencode({'seriesname': name, 'language': 'cs'}))
         try:
-            tvid = re.search('<id>(\d+)</id>', data).group(1);
+            tvid = re.search('<id>(\d+)</id>', data).group(1)
         except:
             if level == 0:
                 tvid = self.getBBDB(name)
             else:
                 tvid = None
         return tvid
-    
+
     def add_item(self, params):
         error = False
         arg = {"play": params['url'], 'cp': 'sosac.ph', "title": params['name']}
         item_url = util._create_plugin_url(arg, 'plugin://' + self.addon_id + '/')
         print("item: ", item_url, params)
         new_items = False
-        #self.showNotification('Linking', params['name'])
-        
+        # self.showNotification('Linking', params['name'])
+
         if "movie" in params['url']:
             item_dir = self.getSetting('library-movies')
-            (error, new_items) = self.add_item_to_library(os.path.join(item_dir, self.normalize_filename(params['name']), self.normalize_filename(params['name'])) + '.strm', item_url)
+            (error, new_items) = self.add_item_to_library(
+                os.path.join(item_dir, self.normalize_filename(params['name']),
+                             self.normalize_filename(params['name'])) + '.strm', item_url)
         else:
             if not ('notify' in params):
                 self.showNotification(params['name'], 'Checking new content')
 
             subs = self.get_subs()
             item_dir = self.getSetting('library-tvshows')
-            
+
             if not params['url'] in subs.keys():
                 subs.update({params['url']: params['name']})
                 self.set_subs(subs)
-                #self.addon.setSetting('tvshows-subs', json.dumps(subs))
-            
-            if not xbmcvfs.exists(os.path.join(item_dir, self.normalize_filename(params['name']), 'tvshow.nfo')):
+                # self.addon.setSetting('tvshows-subs', json.dumps(subs))
+
+            if not xbmcvfs.exists(os.path.join(item_dir, self.normalize_filename(params['name']),
+                                               'tvshow.nfo')):
                 tvid = self.getTVDB(params['name'])
                 if tvid:
-                    self.add_item_to_library(os.path.join(item_dir, self.normalize_filename(params['name']), 'tvshow.nfo'), 'http://thetvdb.com/index.php?tab=series&id=' + tvid)
+                    self.add_item_to_library(os.path.join(item_dir, self.normalize_filename(
+                        params['name']), 'tvshow.nfo'),
+                        'http://thetvdb.com/index.php?tab=series&id=' + tvid)
 
             list = self.provider.list_tv_show(params['url'])
             for itm in list:
-                nfo = re.search('[^\d+](?P<season>\d+)[^\d]+(?P<episode>\d+)', itm['title'], re.IGNORECASE | re.DOTALL)
-                arg = {"play": itm['url'], 'cp': 'sosac.ph', "title": self.normalize_filename(itm['epname'])}
-                #info = ''.join(('<episodedetails><season>',nfo.group('season'),'</season><episode>',nfo.group('episode'),'</episode></episodedetails>'))
+                nfo = re.search('[^\d+](?P<season>\d+)[^\d]+(?P<episode>\d+)',
+                                itm['title'], re.IGNORECASE | re.DOTALL)
+                arg = {"play": itm['url'], 'cp': 'sosac.ph',
+                       "title": self.normalize_filename(itm['epname'])}
+                """
+                info = ''.join(('<episodedetails><season>', nfo.group('season'),
+                                '</season><episode>', nfo.group('episode'),
+                                '</episode></episodedetails>'))
+                """
                 item_url = util._create_plugin_url(arg, 'plugin://' + self.addon_id + '/')
-                (err, new) = self.add_item_to_library(os.path.join(item_dir, self.normalize_filename(params['name']), 'Season ' + nfo.group('season'), "S" + nfo.group('season') + "E" + nfo.group('episode') + '.strm'), item_url)
+                (err, new) = self.add_item_to_library(os.path.join(
+                    item_dir, self.normalize_filename(params['name']), 'Season ' +
+                    nfo.group('season'), "S" + nfo.group('season') + "E" + nfo.group('episode') +
+                    '.strm'), item_url)
                 error |= err
-                if new == True and not err:
+                if new is True and not err:
                     new_items = True
         if not error and new_items and not ('update' in params) and not ('notify' in params):
-            self.showNotification(params['name'],'New content')
+            self.showNotification(params['name'], 'New content')
             xbmc.executebuiltin('UpdateLibrary(video)')
         elif not error and not ('notify' in params):
-            self.showNotification(params['name'],'No new contents')
+            self.showNotification(params['name'], 'No new contents')
         if error and not ('notify' in params):
-            self.showNotification('Failed, Please check kodi.util.info','Linking')
+            self.showNotification('Failed, Please check kodi.util.info', 'Linking')
         return new_items
-    
+
     def run_custom(self, params):
         if 'action' in params.keys():
-            icon = os.path.join(self.addon.getAddonInfo('path'),'icon.png')
+            icon = os.path.join(self.addon.getAddonInfo('path'), 'icon.png')
             if params['action'] == 'remove-subscription':
                 subs = self.get_subs()
                 if params['url'] in subs.keys():
@@ -162,7 +192,7 @@ class XBMCSosac(xbmcprovider.XBMCMultiResolverContentProvider):
                     self.set_subs(subs)
                     self.showNotification(params['name'], 'Removed from subscription')
                 return
-            
+
             if params['action'] == 'add-to-library':
                 return self.add_item(params)
             if params['action'] == 'add-all-to-library':
@@ -174,12 +204,12 @@ class XBMCSosac(xbmcprovider.XBMCMultiResolverContentProvider):
                     self.provider.library_tvshows_all_xml()
                 self.dialog.close()
                 xbmc.executebuiltin('UpdateLibrary(video)')
-            
+
     def add_item_to_library(self, item_path, item_url):
         error = False
         new = False
         if item_path:
-            item_path = xbmc.translatePath( item_path )
+            item_path = xbmc.translatePath(item_path)
             dir = os.path.dirname(item_path)
             if not xbmcvfs.exists(dir):
                 try:
@@ -187,11 +217,11 @@ class XBMCSosac(xbmcprovider.XBMCMultiResolverContentProvider):
                 except Exception, e:
                     error = True
                     print('Failed to create directory 1', dir)
-                    
+
             if not xbmcvfs.exists(dir):
                 error = True
                 print('Failed to create directory 2', dir)
-                
+
             if not xbmcvfs.exists(item_path):
                 try:
                     file_desc = xbmcvfs.File(item_path, 'w')
@@ -203,7 +233,7 @@ class XBMCSosac(xbmcprovider.XBMCMultiResolverContentProvider):
                     error = True
         else:
             error = True
-            
+
         return (error, new)
 
     def get_subs(self):
@@ -214,23 +244,22 @@ class XBMCSosac(xbmcprovider.XBMCMultiResolverContentProvider):
             subs = {}
 
         return subs
-    
+
     def set_subs(self, subs):
         self.cache.set("subscription", repr(subs))
-    
+
     @staticmethod
     def encode(string):
         return unicodedata.normalize('NFKD', string.decode('utf-8')).encode('ascii', 'ignore')
 
     def addon_dir(self):
         return self.addon.getAddonInfo('path')
-    
+
     def data_dir(self):
         return self.addon.getAddonInfo('profile')
-    
+
     def getSetting(self, name):
         return self.addon.getSetting(name)
-    
-    def getString(self,string_id):
-        return self.addon.getLocalizedString(string_id)
 
+    def getString(self, string_id):
+        return self.addon.getLocalizedString(string_id)
