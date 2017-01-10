937d8a44c35746ae0f7c94738cc62ad1a7d3a609
Libor Zoubek <lzoubek@jezzovo.net>
2017-01-10 22:22:23 +0100

    rewrite library support to use JSON files

--- resources/lib/sosac.py
+++ resources/lib/sosac.py
@@ -54,6 +54,18 @@ IMAGE_MOVIE = IMAGE_URL + "75x109/movie-"
 IMAGE_SERIES = IMAGE_URL + "558x313/serial-"
 IMAGE_EPISODE = URL
 
+LIBRARY_MENU_ITEM_ADD = "[B][COLOR red]Add to library[/COLOR][/B]"
+LIBRARY_MENU_ITEM_ADD_ALL = "[B][COLOR red]Add all to library[/COLOR][/B]"
+LIBRARY_MENU_ITEM_REMOVE = "[B][COLOR red]Remove from subscription[/COLOR][/B]"
+LIBRARY_TYPE_VIDEO = "video"
+LIBRARY_TYPE_TVSHOW = "tvshow"
+LIBRARY_TYPE_ALL_VIDEOS = "all-videos"
+LIBRARY_TYPE_ALL_SHOWS = "all-shows"
+LIBRARY_ACTION_ADD = "add-to-library"
+LIBRARY_ACTION_ADD_ALL = "add-all-to-library"
+LIBRARY_ACTION_REMOVE_SUBSCRIPTION = "remove-subscription"
+LIBRARY_FLAG_IS_PRESENT = "[B][COLOR yellow]*[/COLOR][/B] "
+
 RATING = 'r'
 LANG = 'd'
 QUALITY = 'q'
@@ -66,7 +78,8 @@ class SosacContentProvider(ContentProvider):
     def __init__(self, username=None, password=None, filter=None, reverse_eps=False):
         ContentProvider.__init__(self, name='sosac.ph', base_url=MOVIES_BASE_URL, username=username,
                                  password=password, filter=filter)
-        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.LWPCookieJar()))
+        opener = urllib2.build_opener(
+            urllib2.HTTPCookieProcessor(cookielib.LWPCookieJar()))
         urllib2.install_opener(opener)
         self.reverse_eps = reverse_eps
 
@@ -82,25 +95,43 @@ class SosacContentProvider(ContentProvider):
 
     def categories(self):
         result = []
-        for title, url in [
-            ("Movies", URL + J_MOVIES_A_TO_Z_TYPE),
-            ("TV Shows", URL + J_TV_SHOWS_A_TO_Z_TYPE),
-            ("Movies - by Genres", URL + J_MOVIES_GENRE),
-            ("Movies - Most popular", URL + J_MOVIES_MOST_POPULAR),
-            ("TV Shows - Most popular", URL + J_TV_SHOWS_MOST_POPULAR),
-            ("Movies - Recently added", URL + J_MOVIES_RECENTLY_ADDED),
-            ("TV Shows - Recently added", URL + J_TV_SHOWS_RECENTLY_ADDED)]:
-            item = self.dir_item(title=title, url=url)
-#            if title == 'Movies' or title == 'TV Shows' or title == 'Movies - Recently added':
-#                item['menu'] = {"[B][COLOR red]Add all to library[/COLOR][/B]": {
-#                    'action': 'add-all-to-library', 'title': title}}
-            result.append(item)
+        item = self.dir_item(title="Movies", url=URL + J_MOVIES_A_TO_Z_TYPE)
+        item['menu'] = {LIBRARY_MENU_ITEM_ADD_ALL: {
+            'action': LIBRARY_ACTION_ADD_ALL, 'type': LIBRARY_TYPE_ALL_VIDEOS}}
+        result.append(item)
+
+        item = self.dir_item(title="TV Shows", url=URL +
+                             J_TV_SHOWS_A_TO_Z_TYPE)
+        item['menu'] = {LIBRARY_MENU_ITEM_ADD_ALL: {
+            'action': LIBRARY_ACTION_ADD_ALL, 'type': LIBRARY_TYPE_ALL_SHOWS}}
+        result.append(item)
+
+        item = self.dir_item(title="Movies - by Genres", url=URL +
+                             J_MOVIES_GENRE)
+        result.append(item)
+
+        item = self.dir_item(title="Movies - Most popular", url=URL +
+                             J_MOVIES_MOST_POPULAR)
+        result.append(item)
+
+        item = self.dir_item(title="TV Shows - Most popular", url=URL +
+                             J_TV_SHOWS_MOST_POPULAR)
+        result.append(item)
+
+        item = self.dir_item(title="Movies - Recently added", url=URL +
+                             J_MOVIES_RECENTLY_ADDED)
+        result.append(item)
+
+        item = self.dir_item(title="TV Shows - Recently added", url=URL +
+                             J_TV_SHOWS_RECENTLY_ADDED)
+        result.append(item)
+
         return result
 
     def search(self, keyword):
         if len(keyword) < 3 or len(keyword) > 100:
             return [self.dir_item(title="Search query must be between 3 and 100 characters long!", url="fail")]
-        return self.list_search(URL + J_SEARCH + urllib.quote_plus(keyword))
+        return self.list_videos(URL + J_SEARCH + urllib.quote_plus(keyword))
 
     def a_to_z(self, url):
         result = []
@@ -112,18 +143,11 @@ class SosacContentProvider(ContentProvider):
         return result
 
     @staticmethod
-    def remove_flag_from_url(url, flag):
-        return url.replace(flag, "", count=1)
-
-    @staticmethod
     def particular_letter(url):
         return "a-z/" in url
 
     def has_tv_show_flag(self, url):
         return TV_SHOW_FLAG in url
-
-    def remove_flags(self, url):
-        return url.replace(TV_SHOW_FLAG, "", 1)
 
     def list(self, url):
         util.info("Examining url " + url)
@@ -152,7 +176,7 @@ class SosacContentProvider(ContentProvider):
         data = util.request(url)
         json_list = json.loads(data)
         for key, value in json_list.iteritems():
-            item = self.dir_item(title=self.upper_first_letter(key))
+            item = self.dir_item(title=key.title())
             item['url'] = value
             result.append(item)
 
@@ -173,18 +197,29 @@ class SosacContentProvider(ContentProvider):
                 item['lang'] = video[LANG]
             if QUALITY in video:
                 item['quality'] = video[QUALITY]
+            item['menu'] = {LIBRARY_MENU_ITEM_ADD: {
+                'url': item['url'], 'type': LIBRARY_TYPE_VIDEO, 'action': LIBRARY_ACTION_ADD, 'name': self.get_library_video_name(video)}}
+
             result.append(item)
         return result
 
-    def list_series_letter(self, url):
+    def list_series_letter(self, url, load_subs=True):
         result = []
         data = util.request(url)
         json_list = json.loads(data)
+        subs = self.get_subscriptions() if load_subs else {}
         for serial in json_list:
             item = self.dir_item()
             item['title'] = self.get_localized_name(serial['n'])
             item['img'] = IMAGE_SERIES + serial['i']
             item['url'] = serial['l']
+            if item['url'] in subs:
+                item['title'] = LIBRARY_FLAG_IS_PRESENT + item['title']
+                item['menu'] = {LIBRARY_MENU_ITEM_REMOVE: {
+                    'url': item['url'], 'action': LIBRARY_ACTION_REMOVE_SUBSCRIPTION, 'name': self.get_library_video_name(serial)}}
+            else:
+                item['menu'] = {LIBRARY_MENU_ITEM_ADD: {
+                    'url': item['url'], 'type': LIBRARY_TYPE_TVSHOW, 'action': LIBRARY_ACTION_ADD, 'name': self.get_library_video_name(serial)}}
             result.append(item)
         return result
 
@@ -196,8 +231,10 @@ class SosacContentProvider(ContentProvider):
             for series_key, episode in series.iteritems():
                 for episode_key, video in episode.iteritems():
                     item = self.video_item()
-                    item['title'] = series_key + "x" + episode_key + " - " + video['n']
-                    item['img'] = IMAGE_EPISODE + video['i']
+                    item['title'] = series_key + "x" + \
+                        episode_key + " - " + video['n']
+                    if video['i'] is not None:
+                        item['img'] = IMAGE_EPISODE + video['i']
                     item['url'] = video['l'] if video['l'] else ""
                     result.append(item)
         if not self.reverse_eps:
@@ -216,11 +253,36 @@ class SosacContentProvider(ContentProvider):
             result.append(item)
         return result
 
+    def list_all_videos(self):
+        letters = self.load_json_list(URL + J_MOVIES_A_TO_Z_TYPE)
+        total = len(letters)
+
+        step = int(100 / len(letters))
+        for idx, letter in enumerate(letters):
+            for video in self.list_videos(letter['url']):
+                yield video
+            yield {'progress': step * (idx + 1)}
+
+    def list_all_tvshows(self):
+        letters = self.a_to_z(J_TV_SHOWS)
+        total = len(letters)
+
+        step = int(100 / len(letters))
+        for idx, letter in enumerate(letters):
+            for video in self.list_series_letter(letter['url'], False):
+                yield video
+            yield {'progress': step * (idx + 1)}
+
     def get_video_name(self, video):
         name = self.get_localized_name(video['n'])
         year = (" (" + video['y'] + ") ") if video['y'] else " "
         quality = ("- " + video[QUALITY].upper()) if video[QUALITY] else ""
         return name + year + quality
+
+    def get_library_video_name(self, video):
+        name = self.get_localized_name(video['n'])
+        year = (" (" + video['y'] + ") ") if video['y'] else " "
+        return (name + year).encode('utf-8')
 
     def get_episode_recently_name(self, episode):
         serial = self.get_localized_name(episode['t']) + ' '
@@ -229,40 +291,8 @@ class SosacContentProvider(ContentProvider):
         name = self.get_localized_name(episode['n'])
         return serial + series + number + name
 
-    def add_video_flag(self, items):
-        flagged_items = []
-        for item in items:
-            flagged_item = self.video_item()
-            flagged_item.update(item)
-            flagged_items.append(flagged_item)
-        return flagged_items
-
-    def add_directory_flag(self, items):
-        flagged_items = []
-        for item in items:
-            flagged_item = self.dir_item()
-            flagged_item.update(item)
-            flagged_items.append(flagged_item)
-        return flagged_items
-    
     def get_localized_name(self, names):
         return names[self.ISO_639_1_CZECH] if self.ISO_639_1_CZECH in names else names[ISO_639_1_CZECH]
-
-    @cached(ttl=24)
-    def get_data_cached(self, url):
-        return util.request(url)
-
-    def add_flag_to_url(self, item, flag):
-        item['url'] = flag + item['url']
-        return item
-
-    def add_url_flag_to_items(self, items, flag):
-        subs = self.get_subs()
-        for item in items:
-            if item['url'] in subs:
-                item['title'] = '[B][COLOR yellow]*[/COLOR][/B] ' + item['title']
-            self.add_flag_to_url(item, flag)
-        return items
 
     def _url(self, url):
         # DirtyFix nefunkcniho downloadu: Neznam kod tak se toho zkusenejsi chopte
@@ -271,18 +301,6 @@ class SosacContentProvider(ContentProvider):
             return url
         else:
             return self.base_url + "/" + url.lstrip('./')
-
-    def list_tv_shows_by_letter(self, url):
-        util.info("Getting shows by letter " + url)
-        shows = self.list_by_letter(url)
-        util.info("Resolved shows " + str(shows))
-        shows = self.add_directory_flag(shows)
-        return self.add_url_flag_to_items(shows, TV_SHOW_FLAG)
-
-    def list_movies_by_letter(self, url):
-        movies = self.list_by_letter(url)
-        util.info("Resolved movies " + str(movies))
-        return self.add_video_flag(movies)
 
     def resolve(self, item, captcha_cb=None, select_cb=None):
         data = item['url']
@@ -294,11 +312,5 @@ class SosacContentProvider(ContentProvider):
         elif len(result) > 1 and select_cb:
             return select_cb(result)
 
-    def get_subs(self):
+    def get_subscriptions(self):
         return self.parent.get_subs()
-
-    def list_search(self, url):
-        return self.list_videos(url)
-
-    def upper_first_letter(self, name):
-        return name[:1].upper() + name[1:]
